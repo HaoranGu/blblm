b6dcdd179b7a5e1d4eec00578ecf53f27b988fd2
HaoranGu <hrgu@ucdavis.edu>
2020-06-10 16:00:15 -0700

    3

--- tests/testthat/test-fit.R
+++ tests/testthat/test-fit.R
@@ -1,3 +1,7 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("blblm_pl() works", {
+  expect_equivalent(blblm(mpg ~ wt * hp, data = mtcars, m = 3, B = 100, 4), blblm(mpg ~ wt * hp, data = mtcars, m = 3, B = 100, 4))
+})
+
+test_that("lm1 works", {
+  expect_equivalent(lm1(mpg ~ wt * hp , mtcars, NULL), lm(mpg ~ wt * hp , mtcars))
 })
